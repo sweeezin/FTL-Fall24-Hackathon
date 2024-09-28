817870248daf1a7f4a1c9ff9153d5dac9ca26d3e
sweeezin <sweeezin@gmail.com>
2024-09-28 15:40:30 -0700

    OH MY GOD WTF HAPPANED
    use this

---
+++ server.js
@@ -0,0 +1,22 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+    const filePath = path.join(__dirname, 'index.html');
+
+    fs.readFile(filePath, (err, data) => {
+        if (err) {
+            res.writeHead(500, {'Content-Type': 'text/plain'});
+            res.end('Server error');
+            return;
+        }
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end(data);
+    });
+});
+
+const PORT = 8080;
+server.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${8080}`);
+});
